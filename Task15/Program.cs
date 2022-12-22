f03c57e2135954ce353797dbc997388de518addf
Egor <eeeee97345@gmail.com>
2022-12-23 00:50:08 +0300

    completed

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int a = int.Parse(Console.ReadLine());
+
+if (a > 7)
+{
+    Console.WriteLine("Ошибка");
+    return;
+}
+
+if (a == 6 || a == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Не выходной");
+}
